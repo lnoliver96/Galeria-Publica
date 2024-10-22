11c0f23dd03931907c66702d4f5c68b6b80c9742
lnoliver96 <luanotoni4@gmail.com>
2024-10-22 11:13:05 -0300

    Commit até página 23

--- app/src/main/java/otoni/luan/galeriapublica/GalleryRepository.java
+++ app/src/main/java/otoni/luan/galeriapublica/GalleryRepository.java
@@ -12,6 +12,7 @@ import android.provider.MediaStore;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class GalleryRepository {
@@ -27,9 +28,9 @@ public class GalleryRepository {
         int w = (int)context.getResources().getDimension(R.dimen.im_width);
         int h = (int)context.getResources().getDimension(R.dimen.im_height);
 
-        String[] projection = newString[]{MediaStore.Images.Media._ID,
+        String[] projection = new String[]{MediaStore.Images.Media._ID,
             MediaStore.Images.Media.DISPLAY_NAME,
-            MediaStore.Images.DATE_ADDED,
+            MediaStore.Images.Media.DATE_ADDED,
             MediaStore.Images.Media.SIZE};
 
         String selection = null;
@@ -46,11 +47,11 @@ public class GalleryRepository {
                 ContentResolver.QUERY_ARG_SORT_COLUMNS,sort
             );
             queryArgs.putInt(
-                    ContentResolver.QUERY_ARG_SORT_DIRECTION, ContentResolver.QUERY_ARG_SORT_DIRECTION,
+                    ContentResolver.QUERY_ARG_SORT_DIRECTION,
                     ContentResolver.QUERY_SORT_DIRECTION_ASCENDING
             );
             //limit,offset
-            queryArgs.putInt(ContentResolver.QUERY_ARG_LIMIT);
+            queryArgs.putInt(ContentResolver.QUERY_ARG_LIMIT,limit);
             queryArgs.putInt(ContentResolver.QUERY_ARG_OFFSET,offset);
 
             cursor = context.getContentResolver().query(
@@ -66,6 +67,7 @@ public class GalleryRepository {
             cursor = context.getContentResolver().query(
                     MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
                     projection,
+                    selection,
                     selectionArgs,
                     sort + "ASC + LIMIT" + String.valueOf(limit) + "OFFSET" + String.valueOf(offset)
             );
@@ -79,7 +81,7 @@ public class GalleryRepository {
                 long id = cursor.getLong(idColumn);
                 Uri contentUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,id);
                 String name = cursor.getString(nameColumn);
-                int dateAdded = cursor.getString(nameColumn);
+                int dateAdded = cursor.getInt(nameColumn);
                 int size = cursor.getInt(dateAddedColumn);
                 Bitmap thumb = Util.getBitmap(context, contentUri, w, h);
 
--- app/src/main/java/otoni/luan/galeriapublica/MainViewModel.java
+++ app/src/main/java/otoni/luan/galeriapublica/MainViewModel.java
@@ -4,12 +4,24 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.paging.Pager;
+import androidx.paging.PagingConfig;
 
 public class MainViewModel extends AndroidViewModel {
     int navigationOpSelected = R.id.gridViewOp;
 
     public MainViewModel(@NonNull Application application) {
         super(application);
+        GalleryRepository galleryRepository = new GalleryRepository(application);
+        GalleryPagingSource galleryPagingSource = new GalleryPagingSource(galleryRepository);
+        Pager<Integer, ImageData> pager = new Pager(new PagingConfig(10), () -> galleryPagingSource);
+        CoroutineScope viewModelScope = ViewModelKt.getViewModelScope(this);
+        pageLv = PagingLiveData.cachedIn(PagingLiveData.getLiveData(pager), viewModelScope);
+    }
+
+    public LiveData<PagingData<ImageData>> getPageLv(){
+        return pageLv;
     }
 
     public int getNavigationOpSelected() {
